4bb5e975b65f70835b370be0e41966c7b332c3e2
Didik Supriadi <didiksupriadi41@gmail.com>
2020-03-13 02:30:28 +0700

    JSON Requests

--- app/src/main/java/com/example/bolasepak/MainActivity.java
+++ app/src/main/java/com/example/bolasepak/MainActivity.java
@@ -24,9 +24,18 @@ import android.util.Log;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private Context context;
@@ -36,6 +45,11 @@ public class MainActivity extends AppCompatActivity {
     private Intent intent;
     TextView stepCountTV;
     String countedStep;
+    CardViewAdapter cardViewAdapter;
+
+    public static final int LEAGUE_ID = 4328;
+    public static final String LEAGUE_NEXT_EVENT_API="https://www.thesportsdb.com/api/v1/json/1/eventsnextleague.php";
+    public static final String LEAGUE_PAST_EVENT_API="https://www.thesportsdb.com/api/v1/json/1/eventspastleague.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,17 +59,21 @@ public class MainActivity extends AppCompatActivity {
         recycler = findViewById(R.id.recycler_view);
         recycler.setHasFixedSize(true);
 
-        list.addAll(MockData.getListData());
         recycler.setLayoutManager(new LinearLayoutManager(this));
-        CardViewAdapter cardViewAdapter = new CardViewAdapter(this);
-        cardViewAdapter.setListMatch(list);
-        recycler.setAdapter(cardViewAdapter);
+        cardViewAdapter = new CardViewAdapter(this);
+        RequestQueue queue = MySingleton.getInstance(this.getApplicationContext()).
+                getRequestQueue();
+
+        String url = String.format("%s?id=%d", LEAGUE_PAST_EVENT_API, LEAGUE_ID);
+        jsonPastEventRequest(url);
 
         stepCountTV = findViewById(R.id.stepInfo);
         intent = new Intent(this, StepCounterService.class);
         Log.d("SENSORAVAILABLE", String.valueOf(isServiceRunning(StepCounterService.class)));
         if(!isServiceRunning(StepCounterService.class)){
-            startForegroundService(intent);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(intent);
+            }
         }
         registerReceiver(broadcastReceiver, new IntentFilter("com.example.bolasepak.mybroadcast"));
     }
@@ -89,4 +107,85 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
+    public void jsonPastEventRequest(String url) {
+        JsonObjectRequest request = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+
+                            JSONArray pastEvents = response.getJSONArray("events");
+                            for (int i = 0; i < pastEvents.length(); i++) {
+                                SoccerMatch match = new SoccerMatch();
+                                match.setName1( (String) ( (JSONObject) pastEvents.get(i) ).get("strHomeTeam"));
+                                match.setName2( (String) ( (JSONObject) pastEvents.get(i) ).get("strAwayTeam"));
+                                match.setScore1( (String) ( (JSONObject) pastEvents.get(i) ).get("intHomeScore"));
+                                match.setScore2( (String) ( (JSONObject) pastEvents.get(i) ).get("intAwayScore"));
+                                jsonLogo1(match, (String) ( (JSONObject) pastEvents.get(i) ).get("idHomeTeam"));
+                                jsonLogo2(match, (String) ( (JSONObject) pastEvents.get(i) ).get("idAwayTeam"));
+                                list.add(match);
+                            }
+                            cardViewAdapter.setListMatch(list);
+                            recycler.setAdapter(cardViewAdapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }
+                );
+        MySingleton.getInstance(this).addToRequestQueue(request);
+    }
+    public void jsonLogo1(final SoccerMatch match, String id) {
+        String url = "https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=" + id;
+        JsonObjectRequest request = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+
+                            JSONArray pastEvents = response.getJSONArray("teams");
+                            for (int i = 0; i < pastEvents.length(); i++) {
+                                match.setImg1((String) ( (JSONObject) pastEvents.get(i) ).get("strTeamBadge"));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }
+                );
+        MySingleton.getInstance(this).addToRequestQueue(request);
+    }
+    public void jsonLogo2(final SoccerMatch match, String id) {
+        String url = "https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=" + id;
+        JsonObjectRequest request = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+
+                            JSONArray pastEvents = response.getJSONArray("teams");
+                            for (int i = 0; i < pastEvents.length(); i++) {
+                                match.setImg2((String) ( (JSONObject) pastEvents.get(i) ).get("strTeamBadge"));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }
+                );
+        MySingleton.getInstance(this).addToRequestQueue(request);
+    }
 }
