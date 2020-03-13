892cbfe9bfd032477ee35de0590b0858c72a17c8
Didik Supriadi <didiksupriadi41@gmail.com>
2020-03-13 23:58:06 +0700

    Add geo

---
+++ app/src/main/java/com/example/bolasepak/GeocodingLocation.java
@@ -0,0 +1,51 @@
+package com.example.bolasepak;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class GeocodingLocation {
+
+    private static final String TAG = "GeocodingLocation";
+
+    public static void getAddressFromLocation(final String locationAddress,
+                                              final Context context, final Handler handler) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+                String longitude = null;
+                String latitude = null;
+
+                try {
+                    List<Address> addressList = geocoder.getFromLocationName(locationAddress, 1);
+                    if (addressList != null && addressList.size() > 0) {
+                        Address address = addressList.get(0);
+                        longitude = String.valueOf(address.getLongitude());
+                        latitude = String.valueOf(address.getLatitude());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Unable to connect to Geocoder", e);
+                } finally {
+                    Message message = Message.obtain();
+                    message.setTarget(handler);
+                    message.what = 1;
+                    Bundle bundle = new Bundle();
+                    bundle.putString("latitude", latitude);
+                    bundle.putString("longitude", longitude);
+                    message.setData(bundle);
+                    message.sendToTarget();
+                }
+            }
+        };
+        thread.start();
+    }
+}
